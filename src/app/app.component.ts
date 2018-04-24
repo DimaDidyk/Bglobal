637d2806df7c21e569b246e8e619b2389431c591
DimaDidyk <didyk.dima@list.ru>
2018-04-24 04:37:27 +0300

    add jQuery and Swiper slider js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public ngOnInit(){
+  	$(document).ready(function(){
+  		console.log( 'test' );
+  	});
+  }
+  
 }
